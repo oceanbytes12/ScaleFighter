57aefb740362e539dde4e3f0346f36b4eae099e1
desultisoft <desultisoft@gmail.com>
2024-08-17 14:15:15 -0700

    Air Punching implemented

--- HitBox.gd
+++ HitBox.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 var our_body
 var damage = 5
-@onready var animator := $AnimationPlayer
 @onready var collision := $CollisionShape2D
 
 func _ready():
@@ -14,4 +13,4 @@ func _on_body_entered(body):
 		print("hit a body", body.name)
 		if body.has_method("take_hit"):
 			body.take_hit(global_position, damage)
-			collision.disabled = true
+			collision.call_deferred("set_disabled", true)
